bb37daadb0cfe8015b315f82f48a364cf188457d
Hamza Bin Aamir <148682978+Hamza-Bin-Aamir@users.noreply.github.com>
2025-12-15 14:31:48 +0500

    feat: define file descriptor flags and ioctl command structure

--- src/module/mutex_proxy.h
+++ src/module/mutex_proxy.h
@@ -10,15 +10,7 @@
 #define _MUTEX_PROXY_H
 
 #include <linux/types.h>
-
-/* Flags for mutex_proxy_create() syscall */
-#define MUTEX_PROXY_CLOEXEC	(1 << 0)  /* Set close-on-exec */
-#define MUTEX_PROXY_NONBLOCK	(1 << 1)  /* Set O_NONBLOCK */
-#define MUTEX_PROXY_GLOBAL	(1 << 2)  /* Global proxy (all processes) */
-
-#define MUTEX_PROXY_ALL_FLAGS	(MUTEX_PROXY_CLOEXEC | \
-				 MUTEX_PROXY_NONBLOCK | \
-				 MUTEX_PROXY_GLOBAL)
+#include <uapi/linux/mutex_proxy.h>
 
 /* Function prototypes */
 asmlinkage long sys_mutex_proxy_create(unsigned int flags);
